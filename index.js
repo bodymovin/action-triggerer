5d5ec1b3aefa172408a6ec741f2f9112d4f4ff7a
airnan <hernantorrisi@gmail.com>
2022-10-03 17:57:40 -0400

    test

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+async function run() {
+  try {
+    console.log(`PASO 1`);
+    // const token = 'a';
+    const secretToken = process.env.GH_TOKEN || 'none secret';
+    const testToken = process.env.GH_TOKEN || 'none test';
+    console.log('secretToken', secretToken.length);
+    console.log('testToken', testToken.length);
+    // const octokit = github.getOctokit(inputs.token)
+    // console.log(typeof octokit);
+  } catch (error) {
+    console.log('RUN ERROR: ', error);
+  }
+}
+
 try {
-  console.log(`PASO 1`);
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello 2 ${nameToGreet}!`);
-  const printSecret = process.env.SECRET_TEST + '_TEST';
-  const length = process.env.SECRET_TEST.length;
-  const charAt = process.env.SECRET_TEST.charAt(1);
-  console.log(process.env.SECRET_TEST);
-  console.log(printSecret);
-  console.log('length', length);
-  console.log('charAtt', charAt);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  run()
 } catch (error) {
-  core.setFailed(error.message);
   console.log('FAILEDD');
 }
