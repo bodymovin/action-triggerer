a9d20125f489e640ee5a8dab962986146ae8514a
airnan <hernantorrisi@gmail.com>
2022-10-03 20:36:37 -0400

    test

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ async function run() {
     console.log(`PASO 1`);
     // const token = 'a';
     const secretToken = process.env.GH_TOKEN || 'none secret';
-    const testToken = process.env.GH_TOKEN || 'none test';
     console.log('secretToken', secretToken.length);
-    console.log('testToken', testToken.length);
-    // const octokit = github.getOctokit(inputs.token)
-    // console.log(typeof octokit);
+    const octokit = github.getOctokit(secretToken);
+    console.log(typeof octokit);
   } catch (error) {
     console.log('RUN ERROR: ', error);
   }
